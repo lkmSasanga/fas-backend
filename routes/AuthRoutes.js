8dfdc716c1f6ac016df3fab10c41b493d9ee5e43
lkmSasanga <lkmsasanga@gmail.com>
2021-02-04 22:40:37 +0530

    authRoutes added

---
+++ routes/AuthRoutes.js
@@ -0,0 +1,7 @@
+module.exports = function(app) {
+
+    const AuthController = require("../controllers/AuthController");
+
+    app.post("/register", AuthController.registerUser);
+    app.post("/login", AuthController.loginUser);
+};

9b4657e006451dc81010a00785c76ee562e2358e
lkmSasanga <lkmsasanga@gmail.com>
2021-02-04 22:41:47 +0530

    index route added

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/api', function(req, res) {
+    res.send("Welcome to API!");
+});
+
+require('./AuthRoutes')(router);
+
+
+module.exports.router = ro

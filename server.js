5a1d8d78083b735f87d8a43a4633890dd7ce4bce
lkmSasanga <lkmsasanga@gmail.com>
2021-02-11 16:04:30 +0530

    cors added

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 // mongoose.Promise = global.Promise;
+const cors = require('cors');
 require('dotenv').config()
 
 const bodyParser = require('body-parser');
 const app = express()
 const port = process.env.PORT || 5000
 
+app.use(cors());
 app.use(express.json())
 app.use(bodyParser.json());
 
